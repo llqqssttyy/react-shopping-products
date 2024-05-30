811eb313a40357bb15a978f4b2d42ce9aaa3df32
Kim Da Eun <midekuna@gmail.com>
2024-05-30 16:14:39 +0900

    chore: step1 빌드

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,13 +2,16 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from '../App';
 import { PAGE_INFORMATION } from '../constants/page';
 
-const router = createBrowserRouter([
-  {
-    path: PAGE_INFORMATION.main.path,
-    element: <App />,
-    children: [],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: PAGE_INFORMATION.main.path,
+      element: <App />,
+      children: [],
+    },
+  ],
+  { basename: '/react-shopping-products/dist' }
+);
 
 const AppRouter = () => {
   return <RouterProvider router={router} />;
