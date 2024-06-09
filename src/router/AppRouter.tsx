a97603749fd76a72b5ed5982bf68bbf212d58f64
Kim Da Eun <midekuna@gmail.com>
2024-06-09 22:24:24 +0900

    chore: 경로 수정 후 재배포

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -3,18 +3,21 @@ import App from '../App';
 import { PAGE_INFORMATION } from '../constants/page';
 import ProductPage from '../components/page/ProductPage';
 
-const router = createBrowserRouter([
-  {
-    path: '',
-    element: <App />,
-    children: [
-      {
-        path: PAGE_INFORMATION.main.path,
-        element: <ProductPage />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '',
+      element: <App />,
+      children: [
+        {
+          path: PAGE_INFORMATION.main.path,
+          element: <ProductPage />,
+        },
+      ],
+    },
+  ],
+  { basename: '/react-shopping-products/dist' }
+);
 
 const AppRouter = () => {
   return <RouterProvider router={router} />;
